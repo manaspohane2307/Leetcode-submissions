3ce190da690b18bf850a1fd132b667d6ebbbba3e
Manas Pohane <manaspohane@gmail.com>
2025-06-14 18:35:45 +0530

    Time: 768 ms (81.07%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -4,5 +4,5 @@ SELECT p.product_id, IFNULL(ROUND(SUM(p.price*u.units)/SUM(u.units),2),0) AS ave
 FROM Prices p
 LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id
-AND u.purchase_date BETWEEN p.start_date AND p.end_date
+AND u.purchase_date BETWEEN p.start_date AND p.end_date #id where used instead of AND then gives error
 GROUP BY p.product_id
