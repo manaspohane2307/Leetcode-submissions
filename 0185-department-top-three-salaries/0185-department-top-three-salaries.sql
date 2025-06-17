84386741ba7c445aac76bdb0d20e1a2b198083f7
Manas Pohane <manaspohane@gmail.com>
2025-06-17 16:56:50 +0530

    Time: 1224 ms (27.36%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -6,9 +6,10 @@ SELECT
     e1.salary AS Salary
 FROM Employee e1 INNER JOIN Department d
 ON e1.departmentId = d.id
-WHERE 3 > (
+WHERE 3 > ( 
     SELECT COUNT(DISTINCT(e2.salary))
     FROM Employee e2
     WHERE e2.salary > e1.salary AND
     e2.departmentId = e1.departmentId
 )
+#in subquery we are checking that if count is less than 3 means e1.salary is in top3 thus return it
