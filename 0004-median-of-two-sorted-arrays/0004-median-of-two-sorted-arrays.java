9d1d34697485e25e85a95150639e772bb661d13e
Manas Pohane <manaspohane@gmail.com>
2025-07-05 08:33:04 +0530

    Time: 1 ms (100%), Space: 46.2 MB (60.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,46 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int count=0;
-        int total = n+m;
-        int ind2 = total/2;
-        int ind1 = ind2 - 1;
-        int inde1 = -1, inde2 = -1;
+        int n1 = nums1.length, n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);//swap the arrays if n1 is bigger
+        }
+        int n = n1 + n2;//total length
+        int left = (n1 + n2 + 1) / 2;//length of left half
+        //apply binary search
+        int low = 0, high = n1;//range is [0,n1]
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            //calculate l1,l2,r1,r2
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
 
-        int i=0, j=0;
-        while(i<n && j<m){
-            if(nums1[i] < nums2[j]){
-                if(count==ind1) inde1 = nums1[i];
-                if(count==ind2) inde2 = nums1[i];
-                count++;
-                i++;
-            }
-            else{
-                if(count==ind1) inde1 = nums2[j];
-                if(count==ind2) inde2 = nums2[j];
-                count++;
-                j++;
+            if (l1 <= r2 && l2 <= r1) {//correct halves
+                if (n % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
             }
         }
-        //copy left out elements
-        while(i<n){
-            if(count==ind1) inde1 = nums1[i];
-                if(count==ind2) inde2 = nums1[i];
-                count++;
-                i++;
-        }
-        while(j<m){
-            if(count==ind1) inde1 = nums2[j];
-                if(count==ind2) inde2 = nums2[j];
-                count++;
-                j++;
-        }
-        if(total % 2 == 1){
-            return (double)(inde2);
-        }
-        else{
-            return (double)((double)(inde1 + inde2)) / 2.0;
-        }
+        return 0;
     }
 }
