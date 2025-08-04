da03a66e2a9a37733d438452569bf7e8753250dd
Manas Pohane <manaspohane@gmail.com>
2025-08-04 23:20:28 +0530

    Time: 2 ms (36.77%), Space: 43.2 MB (77.97%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -15,39 +15,45 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root==null){
+        if (root == null) {
             return 0;
         }
-        int ans=0;
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
-        q.add(new Pair(root,0));
-        while(!q.isEmpty()){
+        int ans = 0;
+        Queue<Pair> q = new LinkedList<>();//pair stores node and its index
+        q.add(new Pair(root, 0));
+        while (!q.isEmpty()) {
             int size = q.size();
-            int min_id = q.peek().second;
-            int first = 0, last=0;
-            for(int i=0;i<size;i++){
-                int currid = q.peek().second - min_id;
-                TreeNode temp = q.peek().first;
+            int mmin = q.peek().y;//min index in a level
+            int first=0, last=0;
+            for (int i = 0; i < size; i++) {
+                int currid = q.peek().y - mmin;
+                TreeNode node = q.peek().x;
                 q.poll();
-                if(i==0) first = currid;
-                if(i==size-1) last = currid;
-                if(temp.left != null){
-                    q.add(new Pair(temp.left,2*currid+1));
+                if (i == 0) {
+                    first = currid;
                 }
-                if(temp.right != null){
-                    q.add(new Pair(temp.right,2*currid+2));
+                if (i == size - 1) {
+                    last = currid;
+                }
+                if (node.left != null) {
+                    q.add(new Pair(node.left, currid * 2 + 1));
+                }
+                if (node.right != null) {
+                    q.add(new Pair(node.right, currid * 2 + 2));
                 }
             }
-            ans = Math.max(ans,last-first+1); 
+            ans = Math.max(ans, last - first + 1);
         }
         return ans;
     }
 }
-class Pair<K,T>{
-    K first;
-    T second;
-    Pair(K first, T second){
-        this.first = first;
-        this.second = second;
+
+class Pair {
+    TreeNode x;
+    int y;
+
+    Pair(TreeNode x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
